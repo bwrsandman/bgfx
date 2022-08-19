900549a8dea70ef6532f98ec51f9b416b8e06316
Sandy Carter <bwrsandman@gmail.com>
2022-08-19 12:18:46 -0400

    pixelformat example: Use bimg to get format strings

--- examples/47-pixelformats/pixelformats.cpp
+++ examples/47-pixelformats/pixelformats.cpp
@@ -102,49 +102,41 @@ const int HALF_TEXTURE_SIZE = TEXTURE_SIZE / 2;
 
 #pragma pack(push, 1)
 struct A8Pixel {
-    static constexpr const char* str = "A8";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::A8;
     static constexpr const size_t aSz = 8;
     uint8_t a : aSz;
 };
 struct R8Pixel {
-    static constexpr const char* str = "R8";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::R8;
     static constexpr const size_t rSz = 8;
     uint8_t r : rSz;
 };
 struct R8SPixel {
-    static constexpr const char* str = "R8S";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::R8S;
     static constexpr const size_t rSz = 8;
     int8_t r : rSz;
 };
 struct R16Pixel {
-    static constexpr const char* str = "R16";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::R16;
     static constexpr const size_t rSz = 16;
     uint16_t r : rSz;
 };
 struct R16FPixel {
-    static constexpr const char* str = "R16F";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::R16F;
     static constexpr const size_t rSz = 16;
     uint16_t r : rSz;
 };
 struct R16SPixel {
-    static constexpr const char* str = "R16S";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::R16S;
     static constexpr const size_t rSz = 16;
     int16_t r : rSz;
 };
 struct R32FPixel {
-    static constexpr const char* str = "R32F";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::R32F;
     static constexpr const size_t rSz = 32;
     float r;
 };
 struct RG8Pixel {
-    static constexpr const char* str = "RG8";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RG8;
     static constexpr const size_t rSz = 8;
     static constexpr const size_t gSz = 8;
@@ -152,7 +144,6 @@ struct RG8Pixel {
     uint8_t g : gSz;
 };
 struct RGB8Pixel {
-    static constexpr const char* str = "RGB8";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RGB8;
     static constexpr const size_t rSz = 8;
     static constexpr const size_t gSz = 8;
@@ -162,7 +153,6 @@ struct RGB8Pixel {
     uint8_t b : bSz;
 };
 struct RGB8SPixel {
-    static constexpr const char* str = "RGB8S";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RGB8S;
     static constexpr const size_t rSz = 8;
     static constexpr const size_t gSz = 8;
@@ -172,7 +162,6 @@ struct RGB8SPixel {
     int8_t b : bSz;
 };
 struct RGB9E5FPixel {
-    static constexpr const char* str = "RGB9E5";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RGB9E5F;
     static constexpr const size_t rSz = 9;
     static constexpr const size_t gSz = 9;
@@ -184,7 +173,6 @@ struct RGB9E5FPixel {
     uint32_t a : aSz; // shared exponent
 };
 struct RGBA8Pixel {
-    static constexpr const char* str = "RGBA8";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RGBA8;
     static constexpr const size_t rSz = 8;
     static constexpr const size_t gSz = 8;
@@ -196,7 +184,6 @@ struct RGBA8Pixel {
     uint8_t a : aSz;
 };
 struct BGRA8Pixel {
-    static constexpr const char* str = "BGRA8";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::BGRA8;
     static constexpr const size_t rSz = 8;
     static constexpr const size_t gSz = 8;
@@ -208,7 +195,6 @@ struct BGRA8Pixel {
     uint8_t a : aSz;
 };
 struct RGBA8SPixel {
-    static constexpr const char* str = "RGBA8S";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RGBA8S;
     static constexpr const size_t rSz = 8;
     static constexpr const size_t gSz = 8;
@@ -220,7 +206,6 @@ struct RGBA8SPixel {
     int8_t a : aSz;
 };
 struct RGBA16Pixel {
-    static constexpr const char* str = "RGBA16";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RGBA16;
     static constexpr const size_t rSz = 16;
     static constexpr const size_t gSz = 16;
@@ -232,7 +217,6 @@ struct RGBA16Pixel {
     uint16_t a : aSz;
 };
 struct RGBA16FPixel {
-    static constexpr const char* str = "RGBA16F";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RGBA16F;
     static constexpr const size_t rSz = 16;
     static constexpr const size_t gSz = 16;
@@ -244,7 +228,6 @@ struct RGBA16FPixel {
     uint16_t a : aSz;
 };
 struct RGBA16SPixel {
-    static constexpr const char* str = "RGBA16S";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RGBA16S;
     static constexpr const size_t rSz = 16;
     static constexpr const size_t gSz = 16;
@@ -256,7 +239,6 @@ struct RGBA16SPixel {
     int16_t a : aSz;
 };
 struct RGBA32FPixel {
-    static constexpr const char* str = "RGBA32F";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RGBA32F;
     static constexpr const size_t rSz = 32;
     static constexpr const size_t gSz = 32;
@@ -268,7 +250,6 @@ struct RGBA32FPixel {
     float a;
 };
 struct R5G6B5Pixel {
-    static constexpr const char* str = "R5G6B5";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::R5G6B5;
     static constexpr const size_t rSz = 5;
     static constexpr const size_t gSz = 6;
@@ -278,7 +259,6 @@ struct R5G6B5Pixel {
     uint16_t b : bSz;
 };
 struct RGBA4Pixel {
-    static constexpr const char* str = "RGBA4";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RGBA4;
     static constexpr const size_t rSz = 4;
     static constexpr const size_t gSz = 4;
@@ -290,7 +270,6 @@ struct RGBA4Pixel {
     uint16_t a : aSz;
 };
 struct RGB5A1Pixel {
-    static constexpr const char* str = "RGB5A1";
     static constexpr const bgfx::TextureFormat::Enum format = bgfx::TextureFormat::RGB5A1;
     static constexpr const size_t rSz = 5;
     static constexpr const size_t gSz = 5;
@@ -314,32 +293,6 @@ struct RGB10A2Pixel {
     uint32_t a : aSz;
 };
 #pragma pack(pop)
-
-constexpr const char* FormatStrings[] =
-{
-  A8Pixel::str,
-  R8Pixel::str,
-  R8SPixel::str,
-  R16Pixel::str,
-  R16FPixel::str,
-  R16SPixel::str,
-  R32FPixel::str,
-  RG8Pixel::str,
-  RGB8Pixel::str,
-  RGB8SPixel::str,
-  RGB9E5FPixel::str,
-  BGRA8Pixel::str,
-  RGBA8Pixel::str,
-  RGBA8SPixel::str,
-  RGBA16Pixel::str,
-  RGBA16FPixel::str,
-  RGBA16SPixel::str,
-  RGBA32FPixel::str,
-  R5G6B5Pixel::str,
-  RGBA4Pixel::str,
-  RGB5A1Pixel::str,
-  RGB10A2Pixel::str,
-};
 
 /// http://www.rosettacode.org/wiki/Color_wheel#C.2B.2B
 void hsvToRgb(float h, float s, float v, float& r, float& g, float& b) {
@@ -529,7 +482,6 @@ bgfx::TextureHandle createTexture()
                          | BGFX_SAMPLER_MAG_POINT
   ;
   bgfx::TextureHandle handle = bgfx::createTexture2D(TEXTURE_SIZE, TEXTURE_SIZE, false, 1, Pixel::format, flags, mem);
-  bgfx::setName(handle, Pixel::str);
   return handle;
 }
 
@@ -577,33 +529,44 @@ public:
 		// Create program from shaders.
 		m_program = loadProgram("vs_pixelformats", "fs_pixelformats");
 
-    m_textures[0] = createTexture<A8Pixel>();
-    m_textures[1] = createTexture<R8Pixel>();
-    m_textures[2] = createTexture<R8SPixel>();
-    m_textures[3] = createTexture<R16Pixel>();
-    m_textures[4] = createTexture<R16FPixel, true>();
-    m_textures[5] = createTexture<R16SPixel>();
-    m_textures[6] = createTexture<R32FPixel, true>();
-    m_textures[7] = createTexture<RG8Pixel>();
-    m_textures[8] = createTexture<RGB8Pixel>();
-    m_textures[9] = createTexture<RGB8SPixel>();
-    m_textures[10] = createTexture<RGB9E5FPixel>();
-    m_textures[11] = createTexture<BGRA8Pixel>();
-    m_textures[12] = createTexture<RGBA8Pixel>();
-    m_textures[13] = createTexture<RGBA8SPixel>();
-    m_textures[14] = createTexture<RGBA16Pixel>();
-    m_textures[15] = createTexture<RGBA16FPixel, true>();
-    m_textures[16] = createTexture<RGBA16SPixel>();
-    m_textures[17] = createTexture<RGBA32FPixel, true>();
-    m_textures[18] = createTexture<R5G6B5Pixel>();
-    m_textures[19] = createTexture<RGBA4Pixel>();
-    m_textures[20] = createTexture<RGB5A1Pixel>();
-    m_textures[21] = createTexture<RGB10A2Pixel>();
+    int i = 0;
 
-		for (int i = 0; i < sizeof(FormatStrings) / sizeof(FormatStrings[0]); ++i)
-		{
-			bgfx::setViewName(i + 1, FormatStrings[i]);
-		}
+    #define insertTextureFormat(format, floatFormat) \
+      m_textures[i] = createTexture<format##Pixel, floatFormat>(); \
+      m_formats[i] = bgfx::TextureFormat::format; \
+      ++i;
+
+    insertTextureFormat(A8, false);
+    insertTextureFormat(R8, false);
+    insertTextureFormat(R8S, false);
+    insertTextureFormat(R16, false);
+    insertTextureFormat(R16F, true);
+    insertTextureFormat(R16S, false);
+    insertTextureFormat(R32F, true);
+    insertTextureFormat(RG8, false);
+    insertTextureFormat(RGB8, false);
+    insertTextureFormat(RGB8S, false);
+    insertTextureFormat(RGB9E5F, false);
+    insertTextureFormat(BGRA8, false);
+    insertTextureFormat(RGBA8, false);
+    insertTextureFormat(RGBA8S, false);
+    insertTextureFormat(RGBA16, false);
+    insertTextureFormat(RGBA16F, true);
+    insertTextureFormat(RGBA16S, false);
+    insertTextureFormat(RGBA32F, true);
+    insertTextureFormat(R5G6B5, false);
+    insertTextureFormat(RGBA4, false);
+    insertTextureFormat(RGB5A1, false);
+    insertTextureFormat(RGB10A2, false);
+
+    #undef insertTextureFormat
+
+    for (i = 0; i < sizeof(m_textures)/ sizeof(m_textures[0]); ++i)
+    {
+      const char* formatName = bimg::getName(bimg::TextureFormat::Enum(m_formats[i]));
+      bgfx::setName(m_textures[i], formatName); \
+      bgfx::setViewName(i + 1, formatName); \
+    }
 
 		imguiCreate();
 	}
@@ -680,7 +643,7 @@ public:
 				renderScreenSpaceQuad(viewId, m_program, 0.0f, 0.0f, 1280.0f, 720.0f);
 				uint16_t textW = w / 8;
 				uint16_t textH = h / 15;
-				bgfx::dbgTextPrintf(x * textW + (offsetX + textW) / 8, y * textH + (offsetY + textH) / 15, 0x0f, FormatStrings[i]);
+				bgfx::dbgTextPrintf(x * textW + (offsetX + textW) / 8, y * textH + (offsetY + textH) / 15, 0x0f, bimg::getName(bimg::TextureFormat::Enum(m_formats[i])));
 			}
 
 			// Advance to next frame. Rendering thread will be kicked to
@@ -702,7 +665,8 @@ public:
 
 	bgfx::UniformHandle s_texColor;
 	bgfx::ProgramHandle m_program;
-	bgfx::TextureHandle m_textures[sizeof(FormatStrings) / sizeof(FormatStrings[0])];
+	bgfx::TextureFormat::Enum m_formats[22];
+	bgfx::TextureHandle m_textures[sizeof(m_formats) / sizeof(m_formats[0])];
 };
 
 } // namespace
